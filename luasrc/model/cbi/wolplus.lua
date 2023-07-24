ad9831cc3de84fc0fc3a37f6789baf083530495a
Hilman Maulana <hilman0.0maulana@gmail.com>
2023-07-24 23:16:08 +0700

    Update wolplus.lua

--- luasrc/model/cbi/wolplus.lua
+++ luasrc/model/cbi/wolplus.lua
@@ -6,14 +6,18 @@ e = t:section(TypedSection, "macclient", translate("Host Clients"))
 e.template = "cbi/tblsection"
 e.anonymous = true
 e.addremove = true
+---- add device section
 a = e:option(Value, "name", translate("Name"))
 a.optional = false
+---- mac address
 nolimit_mac = e:option(Value, "macaddr", translate("MAC Address"))
 nolimit_mac.rmempty = false
 i.net.mac_hints(function(e, t) nolimit_mac:value(e, "%s (%s)" % {e, t}) end)
+----- network interface
 nolimit_eth = e:option(Value, "maceth", translate("Network Interface"))
 nolimit_eth.rmempty = false
 for t, e in ipairs(i.net.devices()) do if e ~= "lo" then nolimit_eth:value(e) end end
+----- wake device
 btn = e:option(Button, "_awake",translate("Wake Up Host"))
 btn.inputtitle	= translate("Awake")
 btn.inputstyle	= "apply"
